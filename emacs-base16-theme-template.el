77c901dc6154444d2a9f55da2261aebe44b5f3af
Gabriel Fontes <g.fontes@usp.br>
2020-05-25 17:01:48 -0300

    Remove # from color outputs

--- emacs-base16-theme-template.el
+++ emacs-base16-theme-template.el
@@ -5,22 +5,22 @@
 (require 'base16-theme)
 
 (defvar base16-my-auto-colors
-  '(:base00 "{}"
-    :base01 "{}"
-    :base02 "{}"
-    :base03 "{}"
-    :base04 "{}"
-    :base05 "{}"
-    :base06 "{}"
-    :base07 "{}"
-    :base08 "{}"
-    :base09 "{}"
-    :base0A "{}"
-    :base0B "{}"
-    :base0C "{}"
-    :base0D "{}"
-    :base0E "{}"
-    :base0F "{}")
+  '(:base00 "#{}"
+    :base01 "#{}"
+    :base02 "#{}"
+    :base03 "#{}"
+    :base04 "#{}"
+    :base05 "#{}"
+    :base06 "#{}"
+    :base07 "#{}"
+    :base08 "#{}"
+    :base09 "#{}"
+    :base0A "#{}"
+    :base0B "#{}"
+    :base0C "#{}"
+    :base0D "#{}"
+    :base0E "#{}"
+    :base0F "#{}")
   "All colors for Base16 Macoy are defined here.")
 
 ;; Define the theme
